9397cd9e0048e1f21bc39d2928b76f0dce2dc1a8
RahulM-3 <rahulcode97@gmail.com>
2024-11-17 18:04:00 +0530

    basic menu navigation (test)

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -12,10 +12,47 @@ void Application::Initialize(HMODULE _Module)
 
 	ScriptRegister(_Module, []
 	{
+			// Global Variable
+			bool menuActive = false;
+
 			while (true)
 			{
 
+				// Open Trainer
+				if (Input::IsKeyJustPressed(KEY_F4))
+				{
+					menuActive = !menuActive;
+					if (menuActive)
+					{
+						_PRINT_HELP(
+							"<red>NotX Trainer\n"
+							"<purple>>Player\n"
+							"<blue>Teleport\n"
+							"<blue>Spawner\n",
+							10000.0f, true, 1, 0, 0, 0, 0
+						);
+					}
+					else
+					{
+						HUD_CLEAR_HELP_QUEUE();
+					}
+				}
+				
+				// Enter Player options (test)
+				if (menuActive && Input::IsKeyJustPressed(KEY_ENTER))
+				{
+					HUD_CLEAR_HELP_QUEUE();
+					_PRINT_HELP(
+						"<red>NotX Trainer\n"
+						
+						"<purple>>Invincible: " 
+						"<red>OFF\n"
 
+						"<blue>drunk: "
+						"<red>OFF\n",
+						10000.0f, true, 1, 0, 0, 0, 0
+					);
+				}
 
 				logfile.flush();
 				WAIT(0);
